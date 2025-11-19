82ca2561eb668a26dd10bb253e79002c853de2f9
Lizzie <itsalaz@icloud.com>
2025-11-19 06:23:23 -0500

    tool call challenge

---
+++ server.ts
@@ -0,0 +1,46 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
+import { z } from "zod"
+
+
+const server = new McpServer({
+    name: 'Weather Data Fetcher',
+    version: '1.0.0'
+})
+
+
+// define tool
+
+server.tool(
+    'trackPackage',
+    'Track delivery status using tracking number',
+
+{
+    trackingNumber: z.string().describe('Package tracking number')
+},
+
+async ({trackingNumber}) => {
+    return {
+        content: [
+            {
+                type: 'text',
+                text: `Checking delivery status for: ${trackingNumber}`
+            }
+        ]
+    }
+}
+)
+
+async function getWeatherByCity (city: string) {
+    if(city.toLowerCase() === 'new york ') {
+        return { temp: '22°C', forecast: 'Partly cloudy with a breeze'}
+    }
+    if (city.toLowerCase() === 'london') {
+        return { temp: '16°C', forecast: 'Rainy and overcast' }
+    }
+    return { temp: null, error: 'Weather data not available in this city'}
+}
+
+// Challenge: 
+// Create a tool called `getWeatherDataByCityName`
+// It should take a city (New York or London) and return mock weather data as JSON text
+// Use a helper like getWeatherByCity() to return data
