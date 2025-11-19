839797bc15e2af8a70a5da33d2f3a9729d99934d
Lizzie <itsalaz@icloud.com>
2025-11-19 06:45:02 -0500

    get weather by city name

--- server.ts
+++ server.ts
@@ -44,3 +44,24 @@ async function getWeatherByCity (city: string) {
 // Create a tool called `getWeatherDataByCityName`
 // It should take a city (New York or London) and return mock weather data as JSON text
 // Use a helper like getWeatherByCity() to return data
+
+server.tool(
+    'getWeatherByCityName',
+    'Get weather data for New York or London',
+    {
+        city: z.string().describe('Name of the city to get weather for')
+    },
+    async({city}) => {
+        const weatherData = await getWeatherByCity(city)
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: JSON.stringify(weatherData)
+                }
+            ]
+        }
+    }
+)
+
+
